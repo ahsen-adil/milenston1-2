3eb51dd9d211a47e535e58c886651deafa0d5f9b
ahsen adil <meoahsan01@gmail.com>
2024-09-08 06:35:07 +0500

    i have create static resume using typescript html and css

---
+++ script.ts
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+const skillsSection = document.getElementById('skills') as HTMLElement;
+
+toggleButton.addEventListener('click', () => {
+  if (skillsSection.classList.contains('hidden')) {
+    skillsSection.classList.remove('hidden');
+  } else {
+    skillsSection.classList.add('hidden');
+  }
+});
+
